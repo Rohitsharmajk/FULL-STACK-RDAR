2ff8b4fbbf8812bb3c6cd9b16c3daebc5ec6204c
Rohit Sharma <57351783+Rohitsharmajk@users.noreply.github.com>
2023-05-21 09:31:51 +0530

    routing added some modules

---
+++ App.UI/src/app/Models/user.model.ts
@@ -0,0 +1,5 @@
+export  interface UserDto
+    {
+         UserName:string,
+         Password :string
+    }
--- App.UI/src/app/app-routing.module.ts
+++ App.UI/src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { HomeComponent } from './Components/home/home.component';
+import { GetDetailsComponent } from './Components/get-details/get-details.component';
+import { BookFlightComponent } from './Components/book-flight/book-flight.component';
 
 const routes: Routes = [
   // 
-  
+  {
+    path:'',
+    redirectTo:'/login',
+    pathMatch:'full'
+  },
   {
     path:'register',
     component:RegisterComponent
@@ -18,6 +24,14 @@ const routes: Routes = [
   {
     path:'login',
     component:LoginComponent
+  } ,
+  {
+    path:'getDetails',
+    component:GetDetailsComponent
+  } ,
+  {
+    path:'bookFlight',
+    component:BookFlightComponent
   } 
 ];
 
